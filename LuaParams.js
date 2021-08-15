091f4e56ef5c4bc6565cf17d681bd6f2089a2d14
sbxinth <son.son.2013@hotmail.co.th>
2021-08-15 23:18:49 +0700

    sawasdee mo ara

--- LuaParams.js
+++ LuaParams.js
@@ -1,35 +1,39 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class default_1 {
-    constructor(params) {
+exports.__esModule = true;
+var default_1 = /** @class */ (function () {
+    function default_1(params) {
         this.rawParams = [];
         if (params !== undefined) {
             this.add(params);
         }
     }
-    add(params) {
+    default_1.prototype.add = function (params) {
         if (Array.isArray(params)) {
-            for (const p of params) {
+            for (var _i = 0, params_1 = params; _i < params_1.length; _i++) {
+                var p = params_1[_i];
                 this.rawParams.push(p);
             }
         }
         else {
             this.rawParams.push(params);
         }
-    }
-    argvCount() {
+    };
+    default_1.prototype.argvCount = function () {
         return this.rawParams.length;
-    }
-    argvList() {
-        const list = [];
-        for (const p of this.rawParams) {
+    };
+    // { [key: string]: number }[]
+    default_1.prototype.argvList = function () {
+        var list = [];
+        for (var _i = 0, _a = this.rawParams; _i < _a.length; _i++) {
+            var p = _a[_i];
             list.push(p.key);
         }
-        for (const p of this.rawParams) {
+        for (var _b = 0, _c = this.rawParams; _b < _c.length; _b++) {
+            var p = _c[_b];
             list.push(p.argv);
         }
         return list;
-    }
-}
-exports.default = default_1;
-//# sourceMappingURL=LuaParams.js.map
+    };
+    return default_1;
+}());
+exports["default"] = default_1;
